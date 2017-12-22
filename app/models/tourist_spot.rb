8f6024fdd17a5ab23b870285dbae6d84df13aaa2
EC2 Default User <ec2-user@ip-172-31-47-50.us-east-2.compute.internal>
2017-12-22 16:45:50 +0000

    モデル間の関付けの修正

--- app/models/tourist_spot.rb
+++ app/models/tourist_spot.rb
@@ -1,4 +1,4 @@
 class TouristSpot < ApplicationRecord
-    has_many :tourist_days
-    belongs_to :tourist_details, :dependent => :destroy
+    belongs_to :tourist_days
+    has_one :tourist_details, :dependent => :destroy
 end
