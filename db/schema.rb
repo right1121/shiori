fe684848088c9cbe097abe5f3944c6863cac898d
EC2 Default User <ec2-user@ip-172-31-47-50.us-east-2.compute.internal>
2017-12-22 17:02:56 +0000

    競合ファイルの再修正

--- db/schema.rb
+++ db/schema.rb
@@ -29,4 +29,5 @@ ActiveRecord::Schema.define(version: 20171219150153) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
