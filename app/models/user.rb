41825e0730c34b5e4d2e8221f2785723df4e1e89
EC2 Default User <ec2-user@ip-172-31-47-50.us-east-2.compute.internal>
2017-12-27 16:09:19 +0000

    userモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true, length: { maximum: 20 }
   has_many :travel_groups, dependent: :destroy
-  has_many :sioris, through: travel_groups, dependent: :destroy
+  has_many :sioris, through: :travel_groups, dependent: :destroy
 end
