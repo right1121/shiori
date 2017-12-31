10320fd97c5f36a39e8e6ff24e3ac4f98da4c651
EC2 Default User <ec2-user@ip-172-31-47-50.us-east-2.compute.internal>
2017-12-31 12:24:47 +0000

    データベース関連付けの修正

--- db/migrate/20171216143831_create_sioris.rb
+++ db/migrate/20171216143831_create_sioris.rb
@@ -1,14 +1,13 @@
 class CreateSioris < ActiveRecord::Migration[5.1]
   def change
     create_table :sioris do |t|
-      t.integer :user_id,   null: false
+
       t.text    :content
       t.date    :departure_date
       
       t.timestamps
     end
     
-    add_index:sioris, [:user_id]
     add_index:sioris, [:departure_date]
   end
 end
