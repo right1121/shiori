67bd05f8fd09c51998db10ba943f3ad74434a73f
EC2 Default User <ec2-user@ip-172-31-47-50.us-east-2.compute.internal>
2017-12-31 04:58:05 +0000

    ハッシュロケットをシンボルに修正

--- db/migrate/20171216144107_create_travel_groups.rb
+++ db/migrate/20171216144107_create_travel_groups.rb
@@ -1,8 +1,8 @@
 class CreateTravelGroups < ActiveRecord::Migration[5.1]
   def change
     create_table :travel_groups do |t|
-      t.integer :user_id,   :null => false
-      t.integer :siori_id,  :null => false
+      t.integer :user_id,   null: false
+      t.integer :siori_id,  null: false
       
       t.timestamps
     end
--- db/migrate/20171216144142_create_tourist_spots.rb
+++ db/migrate/20171216144142_create_tourist_spots.rb
@@ -1,8 +1,8 @@
 class CreateTouristSpots < ActiveRecord::Migration[5.1]
   def change
     create_table :tourist_spots do |t|
-      t.integer :travel_day_id, :null => false
-      t.integer :order,         :null => false
+      t.integer :travel_day_id, null: false
+      t.integer :order,         null: false
       t.time    :arrival_time
       t.integer :duration
       
