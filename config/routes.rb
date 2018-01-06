30d483ab2dc830122f915b5db4c99706b0894b00
EC2 Default User <ec2-user@ip-172-31-47-50.us-east-2.compute.internal>
2018-01-06 15:34:46 +0000

    masterとのコンフリクト解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< HEAD
-  root 'landing_pages#index'
-  resources :sioris
-=======
   devise_scope :user do
     root :to => "devise/sessions#new"
   end
->>>>>>> master
 end
