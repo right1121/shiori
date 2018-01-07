0df25f980eb872329c5fe311841d2da681f273a9
EC2 Default User <ec2-user@ip-172-31-47-50.us-east-2.compute.internal>
2018-01-07 07:20:06 +0000

    子レコード保存のためモデルの編集

--- app/models/tourist_spot.rb
+++ app/models/tourist_spot.rb
@@ -1,4 +1,5 @@
 class TouristSpot < ApplicationRecord
     belongs_to :tourist_days
-    has_one :tourist_details, dependent: :destroy
+    has_one :tourist_details
+    accepts_nested_attributes_for :tourist_details, allow_destroy: true
 end
--- app/models/travel_day.rb
+++ app/models/travel_day.rb
@@ -1,4 +1,5 @@
 class TravelDay < ApplicationRecord
     belongs_to :siori
-    has_many :tourist_spots, dependent: :destroy
+    has_many :tourist_spots
+    accepts_nested_attributes_for :tourist_spots, allow_destroy: true
 end
