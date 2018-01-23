4dfd24769c790515990dba3ec95dc75e544e3621
EC2 Default User <ec2-user@ip-172-31-47-50.us-east-2.compute.internal>
2018-01-23 16:19:19 +0000

    sioriとtravel_dayのバリデーションを１：多に変更

--- app/models/siori.rb
+++ app/models/siori.rb
@@ -2,6 +2,6 @@ class Siori < ApplicationRecord
     has_many :travel_groups, dependent: :destroy
     has_many :users, through: :travel_groups
     
-    has_one :travel_day
+    has_many :travel_day
     accepts_nested_attributes_for :travel_day, allow_destroy: true
 end
