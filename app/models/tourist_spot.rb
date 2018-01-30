0ee41c677b1e6b16984e36b71b6c2ae6eeea5230
EC2 Default User <ec2-user@ip-172-31-47-50.us-east-2.compute.internal>
2018-01-30 15:39:38 +0000

    順番管理用のコードの削除

--- app/models/tourist_spot.rb
+++ app/models/tourist_spot.rb
@@ -1,7 +1,4 @@
 class TouristSpot < ApplicationRecord
-    include RankedModel
-    ranks :order
-    
     belongs_to :travel_day
     has_one :tourist_detail
     accepts_nested_attributes_for :tourist_detail, allow_destroy: true
