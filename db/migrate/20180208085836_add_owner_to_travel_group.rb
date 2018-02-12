4afe0facf92fd6d326e10c8b9b4a53d858bf4b98
EC2 Default User <ec2-user@ip-172-31-47-50.us-east-2.compute.internal>
2018-02-12 10:06:02 +0000

    travel_groupsのownerカラムを小文字に変更

--- db/migrate/20180208085836_add_owner_to_travel_group.rb
+++ db/migrate/20180208085836_add_owner_to_travel_group.rb
@@ -1,5 +1,5 @@
 class AddOwnerToTravelGroup < ActiveRecord::Migration[5.1]
   def change
-    add_column :travel_groups, :Owner, :boolean, default: false, null: false
+    add_column :travel_groups, :owner, :boolean, default: false, null: false
   end
 end
